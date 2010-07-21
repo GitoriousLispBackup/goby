3f74093ff4f534f6b57f4b7e92a5afa19c2692be
seth <seth@seth-laptop.(none)>
2010-07-20 23:32:43 -0500

    initial abstraction

--- unroll.lisp
+++ unroll.lisp
@@ -1,66 +1,90 @@
 (in-package :goby)
-
-;UTILS
 ;----------------------------------------------------------------------
-(defun prognize (args) (if (length1 args) (first args) `(progn ,@args)))
 (defun constant? (sym) (or (numberp sym) (stringp sym)))
 (defvar *default* '|None|)
+
+(defun set! (var val) `(_= ,var ,val))
+(defun unroll-arg! (form) (unroll form :retv (gensym)))
+(defun unroll-block! (form &optional retv) (unroll form :retv retv :in-block t))
+(defun unroll-all! (form) (mapcar #'unroll form))
+(defun unroll-args! (arguments)
+  (iter (for arg in arguments)
+        (if (constant? arg)
+	    (collecting arg into argument-list)
+	    (let (((ret outer) (unroll-arg arg)))
+	      (collecting ret into argument-list)
+	      (appending outer into outer-code)))
+	(finally (return (values argument-list outer-code)))))
+
+(defmacro defunroll (name arg &rest body) `(defun ,name (,@arg &key retv in-block) ,@body))
+(defmacro defroll (name form-bind &rest body)
+  (let ((form-gen (gensym)) (new-name (symbolicate name "-unroll")))
+    `(defunroll ,new-name (,form-gen)
+       (let ((,form-bind ,form-gen))
+	 ,@body))))
+(defmacro condenv (as-block as-block-and-ret as-embedded)
+  `(cond
+     ((and in-block retv) ,as-block-and-ret)
+     (retv ,as-embedded)
+     (t ,as-block)))
+
+(defmacro call-unroll (arg) `(,(symbolicate arg "-urnoll") :in-block in-block :retv retv))
+(defunroll unroll (form)
+  (cond
+    ((or (atom form) (null form)) (atom-unroll (or form *default*) :in-block in-block :retv retv))
+    ((listp form)
+     (case (first form)
+       (if (call-unroll  if))
+       (progn (call-unroll  progn) ;(progn-unroll form :in-block in-block :retv retv)
+	       )
+       (t (call-unroll  function))))
+    (t (error "what are you?~A" form))))
+(defunroll top-unroll (form)
+  (unroll! form))
 ;----------------------------------------------------------------------
 
+(defroll atom (val)
+  (condenv atom (set! retv atom) atom))
 
-(defun progn-unroll (form &key in-block ret)
-  (let ((non-return (mapcar #'unroll (butlast (cdr form))))
-	(unrolled (unroll (last1 form) :ret ret :in-block t)))
-    (cond
-      ((and in-block ret) `(progn ,@non-return ,unrolled))
-      (ret (values ret `(,@non-return ,unrolled)))
-      (t `(progn ,@non-return ,unrolled)))))
+(defroll progn (_ &rest arguments)
+  (let ((defualt `(progn ,@(unroll-all! (butlast arguments))
+			 ,@(unroll-block! (last1 arguments) retv))))
+    (condenv default default (values retv default))))
 
-(defun function-unroll (form &key in-block ret)
-  (iter
-    (for arg in (cdr form))
-    (if (constant? arg)
-	(collecting arg into function-arguments)
-	(let (((:values unroll-return unroll-body) (unroll arg :ret (gensym))))
-	  (collecting unroll-return into function-arguments)
-	  (appending unroll-body into function-outer)))
-    (finally (let ((default `(,(first form) ,@function-arguments)))	     
-	       (return
-		 (cond
-		   ((and ret (not in-block)) (values default function-outer))
-		   (ret (prognize `(,@function-outer ,(set! ret default))))
-		   (t (prognize `(,@function-outer ,default)))))))))
+(defroll function (name &rest arguments)
+  (let (((arg-list outer-block) (unroll-args! arguments)))
+   (condenv
+    `(progn ,@outer-block (,name ,@arg-list))
+    `(progn ,@outer-block ,(set! retv '(,name ,@arg-list)))
+    (values retv `(progn ,@outer-block ,(set! retv '(,name ,@arg-list)))))))
 
-(defun if-unroll (form &key in-block ret)
-  (let (((test then &optional else) (cdr form))
-	((:values unroll-test outer-test)
-	 (unroll test :in-block nil :ret (gensym)))
-	(unroll-then (unroll then :in-block t :ret ret))
-	(unroll-else (unroll else :in-block t :ret ret))
-	(basic (prognize `(,@outer-test
-			   (if ,unroll-test ,unroll-then ,unroll-else)))))
-    (cond
-      ((and in-block ret) basic)
-      (ret (values ret (list basic)))
-      (t basic))))
+(defroll if (_ test then &optional else)
+  (let (((test-retv test-outer) (unroll-arg! test))
+	(unrolled-then (unroll-block! then))
+	(unrolled-else (unroll-block! else))
+	(default (progn ,@test-outer (if ,test-retv ,unrolled-then ,unrolled-else))))
+   (condenv default default (values retv default))))
 
-(defun atom-unroll (form &key in-block ret)
-  (if (and ret in-block)
-      (set! ret form)
-   form))
+;-------------------------------------------
+;def and class
+(defroll def (_ name args &rest body)
+  (let ((unrolled-body (unroll-blocks! body)))
+    (condenv
+     `(def ,name ,args ,@unrolled-body)
+     `(progn (def ,name ,args ,@unrolled-body) ,(set! retv name))
+     (values name `((def ,name ,args ,@unrolled-body))))))
 
-(defun unroll (form &key in-block ret)
-  (if (and (not (null form)) (listp form))
-      (let ((head (first form)))
-	(cond
-	  ((equalp head 'if)
-	   (if-unroll form :in-block in-block :ret ret))
-	  ((equalp head 'progn)
-	   (progn-unroll form :in-block in-block :ret ret))
-	  (t (function-unroll form :in-block in-block :ret ret))))
-      (atom-unroll (or form *default*) :in-block in-block :ret ret)))
+(defroll class (_ name super &rest body)
+  (let ((unrolled-body (unroll-blocks! body)))
+    (condenv
+     `(class ,name ,super ,@unrolled-body)
+     `(progn (class ,name ,super ,@unrolled-body) ,(set! retv name))
+     (values name `((class ,name ,super ,@unrolled-body))))))
+;--------------------------------------------
 
-(defun top-unroll (form &key (in-block t) ret)
-  (unroll form
-	  :in-block in-block
-	  :ret ret))
+
+;;TODO: implement macros, symbol-macrolets, macrolet, lexical-let, compiler-let
+;;you better make them easy to implement and 'plugin!' (or else!)
+
+
+
--- utils.lisp
+++ utils.lisp
@@ -2,6 +2,14 @@
 (defun last1 (lst) (first (last lst)))
 (defun length1 (lst) (and (not (null lst)) (null (cdr lst))))
 (defun mklst (lst) (if (listp lst) lst (list lst)))
-(defun set! (var val) `(_= ,var ,val))
+
 (defun strcat (&rest args) (apply #'concatenate 'string args))
 
+(define-modify-macro strcatf (&rest args) strcat)
+(defun symbolicate (&rest symbols)
+  (read-from-string
+   (iter
+    (with result = "")
+    (for sym in symbols)
+    (strcatf result (string sym))
+    (finally (return result)))))
