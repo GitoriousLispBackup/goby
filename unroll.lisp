36054f1d7f048009c11c69cc3886e80229a7c41d
seth <seth@seth-laptop.(none)>
2010-07-21 12:12:19 -0500

    fixed progn again and added final pythonic classes. now we can focus on unique macro features, like symbol-macrolet and macrolet

--- unroll.lisp
+++ unroll.lisp
@@ -1,18 +1,31 @@
 (in-package :goby)
+;;(if 2 (progn 3 4) 23)
+;;(or 2 3 4)
+
+(defvar *false* '|False|)
+(defvar *true* '|True|)
+(set-dispatch-macro-character
+ #\# #\f (lambda (stream char1 char2) `(quote ,*false*)))
+(set-dispatch-macro-character
+ #\# #\t (lambda (stream char1 char2) `(quote ,*true*)))
 
 ;----------------------------------------------------------------------
 (defroll atom (value) 
   (condenv value (set! retv value) value))
+(defroll setq ((_ var val))
+  (let (((:values ret outer) (unroll-arg! val)))
+    (condenv
+     `(progn ,@outer ,(set! var ret))
+     `(progn ,@outer ,(set! var ret) ,(set! retv var))
+     (values retv `(,@outer ,(set! var ret) ,(set! retv var))))))
 
 (defunroll macro-unroll (form)
   (unroll (mexpand-all form) :retv retv :in-block in-block))
 
 (defroll progn ((_ &rest arguments))
-  (print arguments)
   (let ((default `(,@(unroll-all! (butlast arguments))
-		     ,(unroll-block! (last1 arguments) retv))))
-    (format t "default is ~A" default)
-    (condenv default default (values retv default))))
+		   ,(unroll-block! (last1 arguments) retv))))
+    (condenv `(progn ,@default) `(progn ,@default) (values retv default))))
 
 (defroll if ((_ test then &optional else))
   (let (((:values test-retv test-outer) (unroll-arg! test))
@@ -51,6 +64,31 @@
   `(progn (_while ,test ,@body) ,*default*))
 (defmac for (clauses &rest body)
   `(progn (_for ,clauses ,@body) ,*default*))
+
+(defmac let (bindings &rest body)
+  `(progn ,@(iter
+	     (for (var val) in bindings)
+	     (collecting `(setq ,var ,val))) ,@body))
+
+;;TODO: make into what a normal python user would use!
+(defmac or (&rest forms &aux (first-gen (gensym)))
+  (if (null forms)
+      #f
+      `(let ((,first-gen ,(first forms)))
+	 (if ,first-gen
+	     ,first-gen
+	     (or ,@(cdr forms))))))
+
+(defmac and (&rest forms &aux (first-gen (gensym)))
+  (if (null forms)
+      #f
+      `(let ((,first-gen ,(first forms)))
+	 (if ,first-gen
+	     (and ,@(cdr forms))
+	     #f))))
+(defmac import (&rest args) `(_import ,@args))
+(defmac from (&rest args) `(_from ,@args))
+
 ;--------------------------------------------
 ;--------------------------------------------
 ;--------------------------------------------
@@ -69,6 +107,7 @@
     ((macro? (first form)) (macro-unroll form :in-block in-block :retv retv))
     (t
      (case (first form)
+       (setq (call-unroll setq))
        (if (call-unroll if))
        (progn  (call-unroll  progn))
        (def (call-unroll def))
