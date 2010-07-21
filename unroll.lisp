67e04a06f17304c8044c00331f332a1cccf2c01c
seth <seth@seth-laptop.(none)>
2010-07-21 11:12:59 -0500

    code for abstraction now officially compiles quite nicely! and works at least with what i testes. moving on to macros and loops....

--- unroll.lisp
+++ unroll.lisp
@@ -4,23 +4,34 @@
 (defvar *default* '|None|)
 
 (defun set! (var val) `(_= ,var ,val))
-(defun unroll-arg! (form) (unroll form :retv (gensym)))
+(defun unroll-arg! (form &optional in-block) (unroll form :retv (gensym) :in-block in-block))
 (defun unroll-block! (form &optional retv) (unroll form :retv retv :in-block t))
+(defun unroll-blocks! (forms &optional retv) (mapcar (lambda (x) (unroll-block! x retv)) forms))
 (defun unroll-all! (form) (mapcar #'unroll form))
-(defun unroll-args! (arguments)
-  (iter (for arg in arguments)
-        (if (constant? arg)
-	    (collecting arg into argument-list)
-	    (let (((ret outer) (unroll-arg arg)))
-	      (collecting ret into argument-list)
-	      (appending outer into outer-code)))
-	(finally (return (values argument-list outer-code)))))
+
+(defun symbol? (s) (symbolp s))
+;;TODO: fix, make more efficient
+(defun unroll-args! (args)
+  (if (every (lambda (x) (or (constant? x) (symbol? x))) args)
+      args
+      (iter
+	(for arg in args)
+	(let (((:values ret outer) (unroll-arg! arg)))
+	  (if (and (symbolp ret) (null outer))
+	      (let ((gen (gensym)))
+		(collecting (set! gen ret) into outer-code)
+		(collecting gen into argument-list))
+	      (progn
+		(appending outer into outer-code)
+		(collecting ret into argument-list))))
+	(finally (return (values argument-list outer-code))))))
+
 
 (defmacro defunroll (name arg &rest body) `(defun ,name (,@arg &key retv in-block) ,@body))
 (defmacro defroll (name form-bind &rest body)
   (let ((form-gen (gensym)) (new-name (symbolicate name "-unroll")))
     `(defunroll ,new-name (,form-gen)
-       (let ((,form-bind ,form-gen))
+       (let ((,@form-bind ,form-gen))
 	 ,@body))))
 (defmacro condenv (as-block as-block-and-ret as-embedded)
   `(cond
@@ -28,61 +39,63 @@
      (retv ,as-embedded)
      (t ,as-block)))
 
-(defmacro call-unroll (arg) `(,(symbolicate arg "-urnoll") :in-block in-block :retv retv))
+
+;----------------------------------------------------------------------
+(defroll atom (value) 
+  (condenv value (set! retv value) value))
+
+(defroll progn ((_ &rest arguments))
+  (let ((default `(progn ,@(unroll-all! (butlast arguments))
+			 ,(unroll-block! (last1 arguments) retv))))
+    (condenv default default (values retv default))))
+
+(defroll if ((_ test then &optional else))
+  (let (((:values test-retv test-outer) (unroll-arg! test))
+	(unrolled-then (unroll-block! then retv))
+	(unrolled-else (unroll-block! else retv))
+	(default `(progn ,@test-outer (if ,test-retv ,unrolled-then ,unrolled-else))))
+   (condenv default default (values retv (list default)))))
+
+(defroll function ((name &rest arguments))
+  (let (((:values arg-list outer-block) (unroll-args! arguments)))
+    (cond
+      ((and (null outer-block) (not in-block)) `(,name ,@arg-list))
+      (t
+       (condenv
+	`(progn ,@outer-block (,name ,@arg-list))
+	`(progn ,@outer-block ,(set! retv `(,name ,@arg-list)))
+	(values retv `(,@outer-block ,(set! retv `(,name ,@arg-list)))))))))
+
+;-------------------------------------------
+;def and class
+(defroll def ((_ name args &rest body))
+  (let ((unrolled-body (unroll-blocks! body)))
+    (condenv
+     `(def ,name ,args ,@unrolled-body)
+     `(progn (def ,name ,args ,@unrolled-body) ,(set! retv name))
+     (values retv `((def ,name ,args ,@unrolled-body) ,(set! retv name))))))
+
+(defroll class ((_ name super &rest body))
+  (let ((unrolled-body (unroll-blocks! body)))
+    (condenv
+     `(class ,name ,super ,@unrolled-body)
+     `(progn (class ,name ,super ,@unrolled-body) ,(set! retv name))
+     (values retv `((class ,name ,super ,@unrolled-body) ,(set! retv name))))))
+;--------------------------------------------
+
+(defmacro call-unroll (arg) `(,(symbolicate arg "-unroll") form :in-block in-block :retv retv))
 (defunroll unroll (form)
   (cond
     ((or (atom form) (null form)) (atom-unroll (or form *default*) :in-block in-block :retv retv))
     ((listp form)
      (case (first form)
        (if (call-unroll  if))
-       (progn (call-unroll  progn) ;(progn-unroll form :in-block in-block :retv retv)
-	       )
-       (t (call-unroll  function))))
-    (t (error "what are you?~A" form))))
+       (progn (call-unroll  progn))
+       (def (call-unroll def))
+       (class (call-unroll class))
+       (t (call-unroll  function))))))
 (defunroll top-unroll (form)
-  (unroll! form))
-;----------------------------------------------------------------------
-
-(defroll atom (val)
-  (condenv atom (set! retv atom) atom))
-
-(defroll progn (_ &rest arguments)
-  (let ((defualt `(progn ,@(unroll-all! (butlast arguments))
-			 ,@(unroll-block! (last1 arguments) retv))))
-    (condenv default default (values retv default))))
-
-(defroll function (name &rest arguments)
-  (let (((arg-list outer-block) (unroll-args! arguments)))
-   (condenv
-    `(progn ,@outer-block (,name ,@arg-list))
-    `(progn ,@outer-block ,(set! retv '(,name ,@arg-list)))
-    (values retv `(progn ,@outer-block ,(set! retv '(,name ,@arg-list)))))))
-
-(defroll if (_ test then &optional else)
-  (let (((test-retv test-outer) (unroll-arg! test))
-	(unrolled-then (unroll-block! then))
-	(unrolled-else (unroll-block! else))
-	(default (progn ,@test-outer (if ,test-retv ,unrolled-then ,unrolled-else))))
-   (condenv default default (values retv default))))
-
-;-------------------------------------------
-;def and class
-(defroll def (_ name args &rest body)
-  (let ((unrolled-body (unroll-blocks! body)))
-    (condenv
-     `(def ,name ,args ,@unrolled-body)
-     `(progn (def ,name ,args ,@unrolled-body) ,(set! retv name))
-     (values name `((def ,name ,args ,@unrolled-body))))))
-
-(defroll class (_ name super &rest body)
-  (let ((unrolled-body (unroll-blocks! body)))
-    (condenv
-     `(class ,name ,super ,@unrolled-body)
-     `(progn (class ,name ,super ,@unrolled-body) ,(set! retv name))
-     (values name `((class ,name ,super ,@unrolled-body))))))
-;--------------------------------------------
-
-
+  (unroll form :in-block t))
 ;;TODO: implement macros, symbol-macrolets, macrolet, lexical-let, compiler-let
 ;;you better make them easy to implement and 'plugin!' (or else!)
 
